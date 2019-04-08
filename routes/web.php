6a9b5c4a4eccea802fc124b324e5706e12123600
Ethan Brace <ethanabrace@gmail.com>
2019-04-08 16:51:09 -0400

    update

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,15 @@ Route::get('/', function () {
 
 Route::get('insta-callback', function(Request $request){
     $client = new \GuzzleHttp\Client();
+    $client_id = 'c49c60f5de314bfc92abd2d128d469e1';
+    $client_secret = '43e24660e0c740b5a2ef61a79ea275f4 ';
+    $redirect = '/inst/authenticated';
+    $code = $request->code;
 
-    dd($client->request('GET', 'https://api.instagram.com/oauth/access_token'));
+    $client->request('GET', "https://api.instagram.com/oauth/access_token?client_id=$client_id&client_secret=$client_secret&grant_type=authorization_code&redirect_uri=$redirect&code=$code");
 //    return redirect()
 });
 
+Route::get('/inst/authenticated', function(Request $request){
+    dd($request->all());
+});
